a677cb9765199bd0743e9c8bc33129b21dd48db9
karan <ks888570@gmail.com>
2023-12-11 18:30:43 +0530

    Updates

---
+++ precache-manifest.1e487f858493375533b1b5503c06b563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5888a94e82cde6f4c956",
+    "url": "/TextUtils/static/css/main.839f4727.chunk.css"
+  },
+  {
+    "revision": "5888a94e82cde6f4c956",
+    "url": "/TextUtils/static/js/main.5888a94e.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/TextUtils/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "ed24c4df8bfca05f303a",
+    "url": "/TextUtils/static/js/2.ed24c4df.chunk.js"
+  },
+  {
+    "revision": "3daee1d7a4a60dc63bef",
+    "url": "/TextUtils/static/js/runtime~main.3daee1d7.js"
+  },
+  {
+    "revision": "a4779b944692c367095e5e9780efb7d5",
+    "url": "/TextUtils/index.html"
+  }
+];
